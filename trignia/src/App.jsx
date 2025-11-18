38f21fdeace2757057c0347022fc48fa9e0ec0bb
J0sePaBl0 <129919106+J0sePaBl0@users.noreply.github.com>
2025-11-18 15:06:16 -0600

    testing git

--- trignia/src/App.jsx
+++ trignia/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import bot from '/bot.svg'
 import './App.css'
 import Spline from '@splinetool/react-spline';
 
@@ -11,7 +12,7 @@ function App() {
   return (
     <>
     <div class = "relative border-4 border-red-500 w-full h-screen">
-      <Spline class="h-full" scene="https://prod.spline.design/PCdkBISceI8jYbUd/scene.splinecode"/>
+      <bot/>
       <div class= "absolute w-full h-1 bottom-2 flex border-2 border-amber-500 bg-amber-700"></div>
     </div>
     </>
